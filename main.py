626ea480d64c53e2c5ee900b38ee0e61b36af58c
SparrowSSSS <vlad22832006@gmail.com>
2023-02-17 09:03:49 +0300

    nocom

--- main.py
+++ main.py
@@ -26,40 +26,143 @@ headers = {
         'Authorization': f"Bearer {token}"
         }
 
-for i in range(1, 3):
+print("---")
+print("Enter the full name of the item without extra characters(russian and english titles supported), if you want to see the entire auction, then write 'all'(will have to wait quite a while).")
+print("---")
+print("If you want to display the category of items, then write the following: 'category:1category(or 2category, if any)', for example: 'category:armor', 'category:combat', нou can also 'category:armor/combat'. To see a list of categories, type 'c'.")
+while True:
+    filter = input(">")
+    if filter == "c":
         print("---")
-        print(i)
-        print("---")
-        new_data=[]
-        if i == 1:
-            data = data_ru
-            region = "ru"
-        else:
-            data = data_g
-            region = "eu"
-        for a in range(len(data)):
-            print(a/len(data)*100)
-            while True:
+        print("List of categories: armor(clothes, combat, combined, device, discoverer, scientist), artefact(biochemical, electrophysical, gravity, other_arts, thermal), attachment(barrel, butt, collimator_sights, forend, mag, other, pistol_handle), backpack, bullet, containers, grenade, medicine, weapon(assault_rifle, device, heavy, machine_gun, melee, pistol, shotgun_rifle, sniper_rifle, submachine_gun)")
+        continue
+    itog = 0
+    if filter == "all":
+        for i in range(1, 3):
+            print("---")
+            print(i)
+            print("---")
+            new_data=[]
+            if i == 1:
+                data = data_ru
+                region = "ru"
+            else:
+                data = data_g
+                region = "eu"
+            for a in range(len(data)):
+                print(a/len(data)*100)
+                while True:
+                    try:
+                        if data[a]['one_category'] == "artefact":
+                            conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots?additional=true", headers=headers)
+                        else:
+                            conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots", headers=headers)
+                        res = conn.getresponse()
+                        break
+                    except:
+                        print("no connection...")
+                d = json.loads(res.read().decode("utf-8"))
                 try:
-                    conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots", headers=headers)
-                    res = conn.getresponse()
-                    break
-                except:
-                    print("no connection...")
-            d = json.loads(res.read().decode("utf-8"))
-            try:
-                if d["total"] > 0:
-                    new_data.append([data[a]["name_ru"], data[a]["name_en"], d, data[a]["icon"], data[a]["category"]])
-            except KeyError:
+                    if d["total"] > 0:
+                        new_data.append([data[a]["name_ru"], data[a]["name_en"], d, data[a]["icon"], data[a]["category"]])
+                except KeyError:
+                    print("---")
+                    print("wait")
+                    time.sleep(60)
+                    print("wait is over")
+                    print("---")
+            if i == 1:
+                new_data_ru = new_data
+            else:
+                new_data_g = new_data
+            itog = 1
+    elif filter != "":
+        if filter[ : 9] == "category:":
+            for i in range(1, 3):
                 print("---")
-                print("wait")
-                time.sleep(60)
-                print("wait is over")
+                print(i)
                 print("---")
-        if i == 1:
-            new_data_ru = new_data
+                new_data=[]
+                if i == 1:
+                    data = data_ru
+                    region = "ru"
+                else:
+                    data = data_g
+                    region = "eu"
+                for a in range(len(data)):
+                    print(a/len(data)*100)
+                    if (data[a]['one_category'] == filter[9 : ]) or (data[a]['two_category'] == filter[9 : ]) or (data[a]['category'] == filter[9 : ]):
+                        while True:
+                            try:
+                                if data[a]['one_category'] == "artefact":
+                                    conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots?additional=true", headers=headers)
+                                else:
+                                    conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots", headers=headers)
+                                res = conn.getresponse()
+                                break
+                            except:
+                                print("no connection...")
+                        d = json.loads(res.read().decode("utf-8"))
+                        try:
+                            if d["total"] > 0:
+                                new_data.append([data[a]["name_ru"], data[a]["name_en"], d, data[a]["icon"], data[a]["category"]])
+                        except KeyError:
+                            print("---")
+                            print("wait")
+                            time.sleep(60)
+                            print("wait is over")
+                            print("---")
+                if i == 1:
+                    new_data_ru = new_data
+                else:
+                    new_data_g = new_data
+            if len(new_data_ru) == 0 and len(new_data_g) == 0:
+                print("---")
+                print("category not found")
+            else:
+                itog = 1
         else:
-            new_data_g = new_data
+            for i in range(1, 3):
+                print("---")
+                print(i)
+                print("---")
+                new_data=[]
+                if i == 1:
+                    data = data_ru
+                    region = "ru"
+                else:
+                    data = data_g
+                    region = "eu"
+                for a in range(len(data)):
+                    print(a/len(data)*100)
+                    if (data[a]['name_ru'] == filter) or (data[a]['name_en'] == filter):
+                        while True:
+                            try:
+                                if data[a]['one_category'] == "artefact":
+                                    conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots?additional=true", headers=headers)
+                                else:
+                                    conn.request("GET", f"/{region}/auction/{data[a]['id']}/lots", headers=headers)
+                                res = conn.getresponse()
+                                break
+                            except:
+                                print("no connection...")
+                        d = json.loads(res.read().decode("utf-8"))
+                        if d["total"] > 0:
+                            new_data.append([data[a]["name_ru"], data[a]["name_en"], d, data[a]["icon"], data[a]["category"]])
+                if i == 1:
+                    new_data_ru = new_data
+                else:
+                    new_data_g = new_data
+            if len(new_data_ru) == 0 and len(new_data_g) == 0:
+                print("---")
+                print("item not found")
+            else:
+                itog = 1
+    else:
+        print("---")
+        print("not recognized")
+    if itog == 1:
+        break
 
 @app.route('/smonitoring')
 def index():
